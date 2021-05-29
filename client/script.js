621fecb195c8b176cbd4fe3d571a1d3a62eaf51d
Luke Harold Miles <luke@cs.uky.edu>
2021-05-29 19:34:42 -0400

    rename div IDs to camelCase and start writing 'fetch repo' button

--- client/script.js
+++ client/script.js
@@ -1,20 +1,19 @@
 "use strict"
 function qs(string) { return document.querySelector(string) }
-const login_holder = qs("#login_holder")
-const logout_button = qs("#logout_button")
-const login_button = qs("#login_button")
-const all_stars_div = qs("#all_stars")
+const loginHolder = qs("#loginHolder")
+const logoutButton = qs("#logoutButton")
+const loginButton = qs("#loginButton")
+const allStarsDiv = qs("#allStars")
 
 function updateLoginButton() {
-    while (login_holder.firstChild) {
-        login_holder.firstChild.remove()
+    while (loginHolder.firstChild) {
+        loginHolder.firstChild.remove()
     } if (window.localStorage.getItem("token") == null) {
-        login_holder.appendChild(login_button)
+        loginHolder.appendChild(loginButton)
     } else {
-        login_holder.appendChild(logout_button)
+        loginHolder.appendChild(logoutButton)
     }
 }
-
 updateLoginButton()
 
 function logout() {
@@ -25,4 +24,4 @@ function logout() {
 let collector = makeDefaultDict((name) => { return { failed: false, done: false, items: [], name: name } })
 
 const someRepos = ["grovesNL", "doppioandante", "anderejd", "fkaa", "doppioslash", "rukai", "nickkuk", "non-descriptive", "ozkriff", "natpbs", "kocsis1david", "kooparse", "phrohdoh", "MarkMcCaskey", "ryanrightmer", "jplatte", "Pacoup", "ColinKinloch", "milkowski", "attliaLin", "bwasty", "mitchmindtree", "unrelentingtech", "khernyo", "danaugrs", "Hossein-Noroozpour", "Atul9", "maeln", "xgalaxy", "rroohhh", "simonrepp", "adamnemecek", "kubo39"]
-batchLoop(someRepos, 'stars', collector).then(() => all_stars_div.innerHTML = "all done!")
+batchLoop(someRepos, 'stars', collector).then(() => allStarsDiv.innerHTML = "all done!")
