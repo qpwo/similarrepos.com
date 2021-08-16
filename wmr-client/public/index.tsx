ffe7d967a0699edeaeb3fc262958b780ffa40d93
Luke Harold Miles <luke@cs.uky.edu>
2021-08-15 21:06:33 -0600

    basic test with localforage seems to work

--- wmr-client/public/index.tsx
+++ wmr-client/public/index.tsx
@@ -1,14 +1,16 @@
 
 import { hydrate, prerender as ssr } from 'preact-iso'
-import { useState } from 'preact/hooks'
+import { useState, useEffect } from 'preact/hooks'
+import localForage from 'localforage'
 
 export function App() {
 	const [x, setX] = useState(0)
+	useEffect(() => { localForage.getItem('x').then(x => setX(x as number)) }, [])
+	useEffect(() => { if (x > 0) localForage.setItem('x', x) }, [x])
 	return <div class="app">
 		<button onClick={() => setX(x => x + 1)}>Inc</button>
 		<div>x is {x}</div>
 	</div>
-
 }
 
 hydrate(<App />)
