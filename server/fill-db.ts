d703b080529f1047c639a6538a7349116972b7f8
Luke Harold Miles <lukem@sent.com>
2022-02-03 20:18:32 -0800

    experimenting

--- server/fill-db.ts
+++ server/fill-db.ts
@@ -1,26 +1,39 @@
 import { AceBase } from 'acebase'
-import { createReadStream } from 'fs'
+import { createReadStream, appendFileSync } from 'fs'
 import { createInterface } from 'readline'
-const db = new AceBase('mydb', { logLevel: 'error' }) // Creates or opens a database with name "mydb"
+// const db = new AceBase('mydb', { logLevel: 'error' }) // Creates or opens a database with name "mydb"
+
+const n = 500_000
 
 const numStargazerRows = 3_139_019
 const numStarsRows = 3_032_978
 const path = '/Users/l/Downloads/github-data'
 
 function log(...args: unknown[]): void {
-    log(new Date().toLocaleString(), ...args)
+    console.log(new Date().toLocaleString(), ...args)
+    appendFileSync(
+        'log.txt',
+        JSON.stringify([new Date().toLocaleString(), ...args])
+    )
 }
 
 void main()
 async function main() {
-    await db.ready()
-    log('db is ready')
+    if (!process.argv[2]) throw Error('must say what kind')
+
+    console.log('APPROACH:', process.argv[2])
+    const start = Date.now()
+    // await db.ready()
+    // log('db is ready')
     // log('loading stars:')
     // await loadStars()
     // log('done loading stars\n\n\n\n\n')
     log('loading gazers:')
-    await loadGazers()
+    await loadGazers(n)
     log('done loading gazers\n\n\n\n\n')
+    const end = Date.now()
+    log('duration:', end - start)
+    log('n was', n)
 }
 
 type MaybeAsync<T> = T extends (...args: infer In) => infer Out
@@ -40,27 +53,32 @@ async function loadStars() {
         // void db.ref(`stars/${user}`).set(stars)
     })
     log('putting in database:')
-    await db.ref('stars').set(giantObj)
+    // await db.ref('stars').set(giantObj)
 }
-async function loadGazers() {
+async function loadGazers(n = -1) {
     let giantObj: Record<string, string[]> = {}
 
-    await processLines(path + '/stargazers.tsv', async (line, num) => {
-        if (num % 10_000 === 0) {
-            log('on line', frac(num, numStargazerRows))
-        }
-        if (num % 10_000 === 0) {
-            log('putting batch in database:')
-            await db.ref('gazers').update(giantObj)
-            log('making new obj hopefully')
-            giantObj = {}
-        }
-        const cols = line.split('\t')
-        const repo = cols[0].replace('/', '!!')
-        const gazers = cols.slice(1)
-        giantObj[repo] = gazers
-        // void db.ref(`gazers/${repo}`).set(gazers)
-    })
+    await processLines(
+        path + '/stargazers.tsv',
+        async (line, num) => {
+            if (num % 10_000 === 0) {
+                log('on line', frac(num, numStargazerRows))
+                // log('number of keys:', Object.keys(giantObj).length)
+            }
+            if (num % 10_000 === 0) {
+                // log('putting batch in database:')
+                // await db.ref('gazers').update(giantObj)
+                // log('making new obj hopefully')
+                // giantObj = {}
+            }
+            const cols = line.split('\t')
+            const repo = cols[0].replace('/', '!!')
+            const gazers = cols.slice(1)
+            giantObj[repo] = gazers
+            // void db.ref(`gazers/${repo}`).set(gazers)
+        },
+        n
+    )
     // await db.ref('gazers').set(giantObj)
 }
 
