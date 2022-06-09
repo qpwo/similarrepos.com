566aff8666dd59eabbb9b8cab4cf7e8c628d4414
Luke <lukem@sent.com>
2022-06-09 01:55:41 +0000

    add script to fill level from tsv

---
+++ useful/fill-level-from-tsv.ts
@@ -0,0 +1,34 @@
+import { createReadStream, readFileSync } from "fs"
+import { ClassicLevel } from "classic-level"
+import { createInterface } from "readline"
+
+const [filename, dbName] =
+    ['stargazers.tsv', 'gazers']
+// ['stars.tsv', 'stars']
+const db_ = new ClassicLevel("db", { valueEncoding: 'json' })
+type Key = string
+
+type Value = string[]
+
+const db = db_.sublevel<Key, Value>(dbName, { valueEncoding: 'json' })
+
+const log = (...args) => console.log(new Date().toLocaleTimeString(), ...args)
+
+main()
+async function main() {
+    log('starting batches')
+    const batch: { type: 'put', key: Key, value: Value }[] = []
+
+    for await (const line of createInterface({
+        input: createReadStream(filename),
+    })) {
+        if (batch.length >= 10000) {
+            void db.batch(batch)
+            batch.length = 0
+            log('did batch')
+        }
+        const [key, ...value] = line.split('\t')
+        batch.push({ type: 'put', key, value })
+
+    }
+}
